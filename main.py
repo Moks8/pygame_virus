a2f61f173937d2cc03c0a09f3573d31e666ba857
Moks8 <cristinagutierrezrub@gmail.com>
2020-06-29 13:25:13 +0200

    set events to hero and set tiled background

--- main.py
+++ main.py
@@ -3,32 +3,75 @@ from pygame.locals import *
 import sys
 from sprites import *
 
+
 BACKGROUND = (50,50,50)
 
 class Game:
     def __init__(self):
+        self.game_over = True
         self.pantalla = pg.display.set_mode((720,400))
         self.pantalla.fill (BACKGROUND)
         self.pantalla.blit(self.pantalla,(0,0))
 
         self.hero = Hero()
-    
+        self.hero_move = 0
+        
+        self.sprites = pg.sprite.Group()
+        self.sprites.add(self.hero)
+
+        self.level = Level()
+
+        self.clock = pg.time.Clock()
+        self.fps = 30
+
+       
 
         
-
         
-
-
+        
         pg.display.set_caption("Covid")
 
-    def main_loop(self):
-        game_over = False
 
-        while not game_over:
+    def handlenEvent(self):
+       pass
+
+    def main_loop(self):
+        if pg.init() == False:
+            self.game_over = False
+
+        while (self.game_over):
             for event in pg.event.get():
                 if event.type == QUIT:
-                    game_over = True
-            self.pantalla.blit(self.hero.image,self.hero.rect.topleft)
+                    self.quit()
+
+                if event.type == KEYDOWN:
+                    if event.key == K_DOWN:
+                        self.hero_move +=10
+                    if event.key == K_UP:
+                        self.hero_move -= 10
+                if event.type == KEYUP:
+                    self.hero_move = 0
+
+                '''
+                key_pressed = pg.key.get_pressed()
+                if key_pressed[K_DOWN]:
+                    self.hero_move -= 1
+                elif key_pressed[K_UP]:
+                    self.hero_move += 1
+                else:
+                    self.hero=0
+                '''
+                
+                
+            self.level.repaint_rect(self.hero.rect)
+            self.hero.rect.centery += self.hero_move
+
+            ms = self.clock.tick(self.fps)
+            self.level.set_ms(ms)
+            #self.pantalla.blit(self.hero.image,self.hero.rect.topleft)
+            self.level.draw(self.pantalla)
+            self.sprites.draw(self.pantalla)
+
             pg.display.flip()
     
     def quit (self):
--- sprites.py
+++ sprites.py
@@ -8,4 +8,53 @@ class Hero(pg.sprite.Sprite):
         self.image = pg.image.load("./resources/hero.png")
         self.rect = self.image.get_rect()
         self.rect.topleft = (32,32)
+        self.hero_move = 0
+
+
+class Tile(pg.sprite.DirtySprite):
+    def __init__(self, image):
+        pg.sprite.DirtySprite.__init__(self)
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.dirty = 1
+
+class Level(pg.sprite.LayeredDirty):
+    def __init__(self):
+        pg.sprite.LayeredDirty.__init__(self)
+        self._data = [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+                      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                      [0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                      [0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                      [0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                      [0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                      [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]]
+        self._back_image = pg.image.load("./resources/arteria_fondo.png")
+        self._limit_sup_image = pg.image.load("./resources/arteria_borde_sup.png")
+        self._limit_inf_image = pg.image.load("./resources/arteria_borde_inf.png")
+
+        for y in range(len(self._data)):
+            for x in range(len(self._data[y])):
+                if self._data[y][x] == 1:
+                    tile = Tile(self._limit_sup_image)
+                elif self._data[y][x] == 2:
+                    tile = Tile(self._limit_inf_image)
+                elif self._data[y][x] == 0:
+                    tile = Tile(self._back_image)
+
+                tile.rect.topleft = (x*32 ,y*32)
+                self.add(tile)
+
+        self.ms_passed = 0
+
+    def set_ms(self,ms):
+        self.ms_passed += ms
+
+
+
+
+
+
         
